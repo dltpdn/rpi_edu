34b188c7cd4e0b2dd6088d4801d6f9e4e83baa94
rainer <rainer@172.30.1.6>
2018-01-11 02:35:48 +0900

    change the path of the location file.

--- weatherlamp/server.js
+++ weatherlamp/server.js
@@ -282,7 +282,7 @@ io.on('connection', function(socket){
 			updateNews();
 		}
 		
-		fs.writeFileSync('/home/pi/weather/location', latitude +','+longitude);
+		fs.writeFileSync('./location', latitude +','+longitude);
 	});
 
 	socket.on('disconnect', function(){
