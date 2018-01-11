db3fda01f5a0ef350936b4ab0d7a5a9fcc77de1a
rainer <rainer@172.30.1.6>
2018-01-11 23:58:12 +0900

    change clock and range value

--- 01gpio/04pwm/servo/servo_wpi_hw.js
+++ 01gpio/04pwm/servo/servo_wpi_hw.js
@@ -7,15 +7,15 @@ var pin = 18;
 wpi.setup('gpio');
 wpi.pinMode(pin, wpi.PWM_OUTPUT);
 wpi.pwmSetMode(wpi.PWM_MODE_MS);
-wpi.pwmSetClock(1920);
-wpi.pwmSetRange(200); //19.2M / 1920 / 200 = 50Hz
+wpi.pwmSetClock(192);
+wpi.pwmSetRange(2000); //19.2M / 192 / 2000 = 50Hz, 1pwm = 0.01ms
 
 rl.setPrompt("1:-90, 2:0, 3: +90 > ");
 
 rl.prompt();
 rl.on('line', function(answer){
-	if(answer ==='1')	wpi.pwmWrite(pin, 5);
-	else if(answer === '2') wpi.pwmWrite(pin,15);
-	else wpi.pwmWrite(pin, 25);
+	if(answer ==='1')	wpi.pwmWrite(pin, 50); // 50*0.01ms = 0.5ms 
+	else if(answer === '2') wpi.pwmWrite(pin,150);
+	else wpi.pwmWrite(pin, 250);
 	rl.prompt();
 }); 
