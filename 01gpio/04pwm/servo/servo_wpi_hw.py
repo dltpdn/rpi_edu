5dcffeb51bd2c78dd8fbba8e078c556c8f435f1d
rainer <rainer@172.30.1.6>
2018-03-06 23:12:03 +0900

    corrected the wrong wpm value and changed divisor and range easier to understand.

--- 01gpio/04pwm/servo/servo_wpi_hw.py
+++ 01gpio/04pwm/servo/servo_wpi_hw.py
@@ -4,14 +4,14 @@ pin = 18
 wpi.wiringPiSetupGpio()
 wpi.pinMode(pin, wpi.PWM_OUTPUT)
 wpi.pwmSetMode(wpi.PWM_MODE_MS)
-wpi.pwmSetClock(192)
-wpi.pwmSetRange(2000)
+wpi.pwmSetClock(384)	#19.2MHz / 384 = 50000Hz = 50KHz(0.02ms)
+wpi.pwmSetRange(1000) # 0.02ms * 1000 = 20ms, 50KHz / 1000 = 50Hz
 
 while True:
 	answer = raw_input("1:-90, 2:0, 3: +90 > ")
 	if answer =='1':
-		wpi.pwmWrite(pin, 5)
+		wpi.pwmWrite(pin, 25)	# 25 * 0.02ms = 0.5ms
 	elif answer == '2':
-		wpi.pwmWrite(pin,15)
+		wpi.pwmWrite(pin, 75)	# 75 * 0.02ms = 1.5ms
 	else:
-		wpi.pwmWrite(pin, 25)
+		wpi.pwmWrite(pin, 125)	# 125 * 0.02ms = 2.5ms
