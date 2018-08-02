fe3e20e18c9884c3be3cfb853514f64ee5d9d8f4
rainer <rainer@172.30.1.6>
2018-08-02 20:03:22 +0900

    support Python3 and OpenCV3

--- 04web_iot/05cam.py
+++ 04web_iot/05cam.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
 cam = cv2.VideoCapture(0)
-cam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320)
-cam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
 @app.route('/')
 def main():
@@ -19,7 +19,7 @@ def oncctv():
     ret, data = cam.read()
     if ret:
         ret, png = cv2.imencode('.png', data)
-        b64 = base64.encodestring(png)
+        b64 = base64.encodestring(png.ravel())
         return b64
     return 'camera is not ready.'
     
