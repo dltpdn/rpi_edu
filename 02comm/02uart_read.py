3081f0ee22adbcb55a7fc6cd1a73b6875cb5f8b1
Mohamad Irfan <irfan.management@gmail.com>
2018-11-23 13:52:38 +0900

    Change baudrate to 9600 & fix carriage return char
    Change baudrate to 9600 to match arduino baudrate and fixing carriage return '\r' typo

--- 02comm/02uart_read.py
+++ 02comm/02uart_read.py
@@ -1,6 +1,6 @@
 import serial
 
-port = serial.Serial("/dev/serial0", baudrate=115200, timeout=3.0)
+port = serial.Serial("/dev/serial0", baudrate=9600, timeout=3.0)
 
 
 def readline():
@@ -8,7 +8,7 @@ def readline():
     while True:
         ch = port.read().decode()
         line +=ch
-        if ch =='\n' or ch == 'r' or ch == '':
+        if ch =='\n' or ch == '\r' or ch == '':
             if len(line) > 0 :
                 return line
     
