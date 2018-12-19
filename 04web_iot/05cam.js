c3842d3aa477f808d2b871795ca9cfc9e8914484
sewoo lee <rainer@ip-70-12-113-219.kscymo.spcsdns.net>
2018-12-19 19:11:56 +0900

    modify cam.js

--- 04web_iot/05cam.js
+++ 04web_iot/05cam.js
@@ -4,8 +4,8 @@ var app = express();
 
 
 var camera = new cv.VideoCapture(0);
-camera.setWidth(320);
-camera.setHeight(240);
+//camera.setWidth(320);
+//camera.setHeight(240);
 
 app.use(express.static(__dirname + '/static'));
 app.get('/', function(req, res){
@@ -15,16 +15,22 @@ app.get('/', function(req, res){
 app.get('/cctv', function(req, res){
 	console.log(req.url, req.query);
     camera.read(function(err, im) {
-        if (err) throw err;
-        else{
-        	var buff = im.toBuffer();
-        	var b64 = buff.toString('base64');
-        	console.log(buff.length);
-        	res.send(b64);
+        if (err){
+        	console.log("Error", err);
+        	res.sendStatus(500);
+        }else{
+        	console.log(im.size());
+        	if (im.size()[0] > 0 && im.size()[1] > 0){
+	        	var buff = im.toBuffer();
+	        	var b64 = buff.toString('base64');
+	        	console.log(buff.length);
+	        	res.send(b64);
+        	}else{
+        		res.sendStatus(500);
+        	}
         }
       });	
 });
-
 
 app.listen(5000, function(){
 	console.log('server running on 5000.');
