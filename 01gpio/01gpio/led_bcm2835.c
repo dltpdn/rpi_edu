f97c80d12a6ee5ac045021995101cfec6ffc6a80
dltpdn <dltpdn@gmail.com>
2020-12-23 12:49:24 +0900

    chage gpio pin form 17 to 18

--- 01gpio/01gpio/led_bcm2835.c
+++ 01gpio/01gpio/led_bcm2835.c
@@ -1,24 +1,23 @@
 #include <bcm2835.h>
 #include <stdio.h>
-// Blinks on RPi Plug P1 pin 11 (which is GPIO pin 17)
-#define PIN RPI_GPIO_P1_11
+
+#define PIN RPI_GPIO_P1_12 //(which is GPIO 18)
 int main(int argc, char **argv){
     if (!bcm2835_init())      return 1;
     bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
-    while (1)
-    {
+    int i;
+    for(i=0; i<10; i++){
         // Turn it on
         bcm2835_gpio_write(PIN, HIGH);
-
+        printf("pin %d HIGH\n" , PIN);
         // wait a bit
         bcm2835_delay(500);
-
         // turn it off
         bcm2835_gpio_write(PIN, LOW);
-
+        printf("pin %d Low\n" , PIN);
         // wait a bit
         bcm2835_delay(500);
     }
-    bcm2835_close();
+    bcm2835_close(); 
     return 0;
 }
