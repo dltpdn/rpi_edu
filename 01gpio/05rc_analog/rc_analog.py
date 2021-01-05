ea7178bb5c0104c024b6d8ccfcd6d2f6a622af9c
dltpdn <dltpdn@gmail.com>
2021-01-05 10:46:20 +0900

    organize comments

--- 01gpio/05rc_analog/rc_analog.py
+++ 01gpio/05rc_analog/rc_analog.py
@@ -8,15 +8,17 @@ PIN_MEASURE = 23
 GPIO.setmode(GPIO.BCM)
 
 while True:
-    GPIO.setup(PIN_CHARGE, GPIO.IN) #lock charging
+    # for discharge
+    GPIO.setup(PIN_CHARGE, GPIO.IN) 
     GPIO.setup(PIN_MEASURE, GPIO.OUT)
-    GPIO.output(PIN_MEASURE, False) #making GND, discharge
-    time.sleep(0.1) #waiting for discharge
+    GPIO.output(PIN_MEASURE, False) 
+    time.sleep(0.1) 
     
-    GPIO.setup(PIN_MEASURE, GPIO.IN) #switching GND to measuring pin
-    GPIO.setup(PIN_CHARGE, GPIO.OUT) #making charge
-    count = 0                       #reset counter
-    GPIO.output(PIN_CHARGE, True)  #start charging
-    while not GPIO.input(PIN_MEASURE):  #until recognize
-        count = count + 1           #accumulate count
+    # for charging and measuring
+    GPIO.setup(PIN_MEASURE, GPIO.IN) 
+    GPIO.setup(PIN_CHARGE, GPIO.OUT) 
+    count = 0                       
+    GPIO.output(PIN_CHARGE, True)  
+    while not GPIO.input(PIN_MEASURE):  
+        count = count + 1           
     print(count)
