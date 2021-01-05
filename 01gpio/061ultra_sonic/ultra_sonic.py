2d7b7bce2de29430f73bed37574bd7d852ce64d4
dltpdn <dltpdn@gmail.com>
2021-01-05 11:50:45 +0900

    organize source

--- 01gpio/061ultra_sonic/ultra_sonic.py
+++ 01gpio/061ultra_sonic/ultra_sonic.py
@@ -4,29 +4,30 @@ import time
 PIN_TRIG = 18
 PIN_ECHO = 23 
 
-distance = 0
+dist, old_dist = 0,0
 start_time = 0
-end_time=0
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_TRIG, GPIO.OUT)
+GPIO.setup(PIN_ECHO, GPIO.IN)
 
 try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(PIN_TRIG, GPIO.OUT)
-    GPIO.setup(PIN_ECHO, GPIO.IN)
-    
     while True:
-        GPIO.output(PIN_TRIG, False)
-        time.sleep(0.2)     #wait for being ready
         GPIO.output(PIN_TRIG, True)
-        time.sleep(0.00001)  #set HIGH for 10us
+        time.sleep(0.00001)  # 10us
         GPIO.output(PIN_TRIG, False)
         while GPIO.input(PIN_ECHO) == 0:
-            start_time = time.time()
+            pass
+        start_time = time.time()
         while GPIO.input(PIN_ECHO) == 1:
-            end_time = time.time()
-        travel_time = end_time - start_time;
-        distance = travel_time * 17160 #34321/2
-        distance = round(distance, 2)
-        print('Distance:%dcm' %distance)
+            pass
+        travel_time = time.time() - start_time
+        dist = travel_time * 17160 #34321/2
+        dist = round(dist)
+        if dist != old_dist:
+            old_dist = dist
+            print(f'Distance:{dist}cm')
+        time.sleep(0.01)
 finally:
     print('Clean up')
     GPIO.cleanup()                
--- 01gpio/061ultra_sonic/ultra_sonic_wpi.py
+++ 01gpio/061ultra_sonic/ultra_sonic_wpi.py
@@ -2,20 +2,20 @@ import wiringpi as wpi, time
 PIN_TRIG = 18
 PIN_ECHO = 23
 
-wpi.wiringPiSetupGpio();
-wpi.pinMode(PIN_TRIG, wpi.OUTPUT);
-wpi.pinMode(PIN_ECHO, wpi.INPUT);
+wpi.wiringPiSetupGpio()
+wpi.pinMode(PIN_TRIG, wpi.OUTPUT)
+wpi.pinMode(PIN_ECHO, wpi.INPUT)
 
 while True:
-	wpi.digitalWrite(PIN_TRIG, wpi.LOW)
-	wpi.delay(200);
 	wpi.digitalWrite(PIN_TRIG, wpi.HIGH)
 	wpi.delayMicroseconds(10)
 	wpi.digitalWrite(PIN_TRIG, wpi.LOW)
 	while wpi.digitalRead(PIN_ECHO) == 0:
-		start_time = time.time()
+		pass
+	start_time = time.time()
 	while wpi.digitalRead(PIN_ECHO) == 1:
-		end_time = time.time()
-	travel_time = end_time - start_time
-	distance = travel_time * 17160;
+		pass
+	travel_time = time.time() - start_time
+	distance = travel_time * 17160
 	print('Distance: %dCm '%round(distance, 2)  , travel_time)
+	wpi.delay(10)
